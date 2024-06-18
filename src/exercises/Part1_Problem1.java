d321dabb34bcb617d35a956bed983bed1fbb0ec1
adrian.urate <adrian.calin991@gmail.com>
2024-06-18 19:42:27 +0300

    Finished the first problem

--- src/exercises/Part1_Problem1.java
+++ src/exercises/Part1_Problem1.java
@@ -6,10 +6,15 @@ public class Part1_Problem1 {
         //Is it even or odd? Ask the user for an integer and print whether it's even or odd.
         System.out.println("Enter a number : ");
 
+        //Reading the input from user
         Scanner input = new Scanner(System.in);
         String valueFromUser = input.next();
+        //End of reading
+
+        //Store the input in variable
         int userInput = Integer.parseInt(valueFromUser);
 
+        //Verification
         if(userInput % 2 == 0){
             System.out.println(userInput + ", is even.");
         }
--- src/exercises/Part1_Problem2.java
+++ src/exercises/Part1_Problem2.java
@@ -6,5 +6,21 @@ public class Part1_Problem2 {
     public static void main(String[] args) {
         // Positive, Negative, or Zero? Ask the user for a number and print if it's positive, negative, or zero.
         System.out.println("Enter a number : ");
+
+        //Reading the input from user
+        Scanner input = new Scanner(System.in);
+        String valueFromUser = input.next();
+        //End of reading
+
+        //Store the input in variable
+        int userInput = Integer.parseInt(valueFromUser);
+
+        //Verification
+        if (userInput == 0)
+                System.out.println("The value is equal to 0");
+        else if (userInput > 0)
+            System.out.println("The value is positive");
+        else
+            System.out.println("The value is negative");
     }
 }
