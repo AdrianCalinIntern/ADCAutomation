df285a371d7550e6670e0e50df6c22f9fe4e1655
adrian.urate <adrian.calin991@gmail.com>
2024-06-18 19:36:05 +0300

    Finished the first problem

---
+++ src/exercises/Part1_Problem2.java
@@ -0,0 +1,10 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Part1_Problem2 {
+    public static void main(String[] args) {
+        // Positive, Negative, or Zero? Ask the user for a number and print if it's positive, negative, or zero.
+        System.out.println("Enter a number : ");
+    }
+}
