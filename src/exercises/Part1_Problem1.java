193189671fd328c43a67954aa890421eb63ad979
adrian.urate <adrian.calin991@gmail.com>
2024-06-18 19:24:21 +0300

    Finished the first problem

---
+++ src/exercises/Part1_Problem1.java
@@ -0,0 +1,20 @@
+package exercises;
+import java.util.*;
+
+public class Part1_Problem1 {
+    public static void main(String[] args) {
+        //Is it even or odd? Ask the user for an integer and print whether it's even or odd.
+        System.out.println("Enter a number : ");
+
+        Scanner input = new Scanner(System.in);
+        String valueFromUser = input.next();
+        int userInput = Integer.parseInt(valueFromUser);
+
+        if(userInput % 2 == 0){
+            System.out.println(userInput + ", is even");
+        }
+        else{
+            System.out.println(userInput + ", is odd");
+        }
+    }
+}
