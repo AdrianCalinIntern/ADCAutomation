81d1e359d503aff505381e7bf69ca53e7ff877c4
adrian.urate <adrian.calin991@gmail.com>
2024-06-20 18:36:05 +0300

    Live Session

--- src/exercises/LiveSession.java
+++ src/exercises/LiveSession.java
@@ -25,78 +25,83 @@ public class LiveSession {
 
 
         Scanner input = new Scanner(System.in);
-        //User menu
-        System.out.println("1 Check the Balance  \n2 Deposit money  \n3 Withdraw money \n4 Transfer money to a savings account \n5 Convert account balance into USD or EUR \n6 Pay and stash change");
-
-        //read the user input
-        int userInput = input.nextInt();
 
         //declaring accountBalance and depositBalance
         double accountBalance = 10;
         double depositBalance = 5;
+        int userInput = 0;
 
+        while(userInput != 7 ){
+            System.out.println("1 Check the Balance  \n2 Deposit money  \n3 Withdraw money \n4 Transfer money to a savings account \n5 Convert account balance into USD or EUR \n6 Pay and stash change \n7 Exit");
+            //read the user input
+            userInput = input.nextInt();
 
-        if (userInput == 1) {
-            System.out.println("Current balance account : " + accountBalance);
-        }
-
-        else if (userInput == 2) {
-            System.out.println("Amount to add :");
-            double ammountToAdd = input.nextDouble();
-            accountBalance = accountBalance + ammountToAdd;
-            System.out.println("Your new balance : " + accountBalance);
-        }
-
-        else if (userInput == 3) {
-            System.out.println("Value to transfer to another account: ");
-            float transferToAnotherAccount = input.nextFloat();
-            if (transferToAnotherAccount > accountBalance)
-            {
-                System.out.println("Insufficient found");
-            } else
-            {
-                accountBalance = accountBalance - transferToAnotherAccount;
-                System.out.println("Your new balance : " + accountBalance);
+            if (userInput == 1) {
+                System.out.println("Current balance account : " + accountBalance + "\n");
             }
-        }
 
-        else if (userInput == 4){
-            System.out.println("Value to transfer to saving account:  ");
-            double transferToSavingAccount = input.nextFloat();
-            if (transferToSavingAccount > accountBalance)
-            {
-                System.out.println("Insufficient found");
+            else if (userInput == 2) {
+                System.out.println("Amount to add :");
+                double ammountToAdd = input.nextDouble();
+                accountBalance = accountBalance + ammountToAdd;
+                System.out.println("Your new balance : " + accountBalance + "\n");
             }
-            else {
-                accountBalance = accountBalance - transferToSavingAccount;
-                depositBalance = depositBalance + transferToSavingAccount;
-                System.out.println("New account balance: " + accountBalance);
-                System.out.println("New deposit balance: " + depositBalance);
+
+            else if (userInput == 3) {
+                System.out.println("Value to transfer to another account: ");
+                float transferToAnotherAccount = input.nextFloat();
+                if (transferToAnotherAccount > accountBalance)
+                {
+                    System.out.println("Insufficient found");
+                } else
+                {
+                    accountBalance = accountBalance - transferToAnotherAccount;
+                    System.out.println("Your new balance : " + accountBalance+ "\n");
+                }
             }
-        }
 
-        else if (userInput == 5){
-            System.out.println("Select : \n 1 for USD \n 2 for EUR");
-            int option = input.nextInt();
-
-            if(option == 1)
-                System.out.println("Account Balance in USD: " + accountBalance / 4.63);
-            else if (option == 2)
-                System.out.println("Account Balance in EUR: " + accountBalance / 0.93);
-            else System.out.println("Invalid option");
-        }
-
-        else if (userInput == 6) {
-            System.out.println("Payment amount : ");
-            double paymentAmount = input.nextDouble();
-            accountBalance = accountBalance - paymentAmount;
-
-            double change = accountBalance - Math.floor(accountBalance);
-            if (change > 0){
-                depositBalance = depositBalance + change;
-                System.out.println(depositBalance);
+            else if (userInput == 4){
+                System.out.println("Value to transfer to saving account:  ");
+                double transferToSavingAccount = input.nextFloat();
+                if (transferToSavingAccount > accountBalance)
+                {
+                    System.out.println("Insufficient found");
+                }
+                else {
+                    accountBalance = accountBalance - transferToSavingAccount;
+                    depositBalance = depositBalance + transferToSavingAccount;
+                    System.out.println("New account balance: " + accountBalance+ "\n");
+                    System.out.println("New deposit balance: " + depositBalance+ "\n");
+                }
             }
+
+            else if (userInput == 5){
+                System.out.println("Select : \n 1 for USD \n 2 for EUR");
+                int option = input.nextInt();
+
+                if(option == 1)
+                    System.out.println("Account Balance in USD: " + accountBalance / 4.63+ "\n");
+                else if (option == 2)
+                    System.out.println("Account Balance in EUR: " + accountBalance / 0.93+ "\n");
+                else System.out.println("Invalid option"+ "\n");
+            }
+
+            else if (userInput == 6) {
+                System.out.println("Payment amount : ");
+                double paymentAmount = input.nextDouble();
+                accountBalance = accountBalance - paymentAmount;
+
+                double change = accountBalance - Math.floor(accountBalance);
+                if (change > 0){
+                    depositBalance = depositBalance + change;
+                    System.out.println(depositBalance);
+                }
+            }
+            else if (userInput == 7) {
+                System.out.println("Bye");
+            }
+            else System.out.println("Invalid choose !");
+
         }
-        else System.out.println("Invalid choose !");
         }
     }
